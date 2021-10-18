57e0254e8b76dc3a582fdb7848e028f33acf802e
vann4oto98 <vanyo.ivanov@profilink.eu>
2021-10-18 11:44:26 +0300

    feat: initial webpack tsx/css/svg config

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+
+module.exports = {
+    entry: './src/index.tsx',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'] //They load in reverse order
+            },
+            {
+                test: /\.svg$/,
+                use: [
+                    {
+                        loader: 'svg-url-loader',
+                        options: {
+                            limit: 10000,
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist")
+    }
+};
